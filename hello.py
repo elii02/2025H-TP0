c92f3df4d74345133d204c2ee5bcc10286c7a5d3
Elisabeth KJ <elisabeth.kerr-jarrold@polymtl.ca>
2025-01-14 17:32:24 -0500

    test 0

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+aire = math.pi*rayon*rayon
+print(aire)
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print('eli')
